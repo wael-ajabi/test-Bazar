5536fe7c84fffc891cb8bbf7d46a2f220a4e00a1
wael-ajabi <90458716+wael-ajabi@users.noreply.github.com>
2022-04-17 11:51:36 +0100

    new model test

--- src/script.js
+++ src/script.js
@@ -1,139 +1,202 @@
-
-import * as THREE from 'three';
-
-import { GUI } from 'three/examples/jsm/libs/lil-gui.module.min.js';
-
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
-import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
-import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+import './style.css'
+import * as THREE from 'three'
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Sky } from "three/examples/jsm/objects/Sky.js";
+import * as dat from "dat.gui";
 
-let camera, stats;
-let composer, renderer, mixer, clock;
+// Debug
+const gui = new dat.GUI();
 
-const params = {
-    exposure: 1,
-    bloomStrength: 1.5,
-    bloomThreshold: 0,
-    bloomRadius: 0
-};
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
 
+// Scene
+const scene = new THREE.Scene()
+scene.background = new THREE.Color(0xdddddddd);
+// var fog = new THREE.FogExp2( 0xefd1b5, 0.08 );
+// scene.fog = fog
 
-
-    const container = document.getElementById( 'container' );
-
-    clock = new THREE.Clock();
-
-    renderer = new THREE.WebGLRenderer( { antialias: true } );
-    // renderer.setClearColor( 0x101000 );
-    renderer.autoClear = false;
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.toneMapping = THREE.ReinhardToneMapping;
-    container.appendChild( renderer.domElement );
-
-    const scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 300 );
-    camera.layers.enable(1);
-    camera.position.set( - 9, 15, - 80 );
-    scene.add( camera );
-
-    const controls = new OrbitControls( camera, renderer.domElement );
-    controls.maxPolarAngle = Math.PI * 0.5;
-    controls.minDistance = 1;
-    controls.maxDistance = 200;
-
-    scene.add( new THREE.AmbientLight( 0x404040 ) );
-
-    const pointLight = new THREE.PointLight( 0xffffff, 1 );
-    camera.add( pointLight );
-
-    const renderScene = new RenderPass( scene, camera );
-
-    const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
-    bloomPass.threshold = params.bloomThreshold;
-    bloomPass.strength = params.bloomStrength;
-    bloomPass.radius = params.bloomRadius;
-    const gltfloader = new GLTFLoader()
-
-    composer = new EffectComposer( renderer );
-    composer.addPass( renderScene );
-    composer.addPass( bloomPass );
-    var dracoLoader = new DRACOLoader();
-    dracoLoader.setDecoderPath('https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/js/libs/draco/');
-    dracoLoader.preload();
-    gltfloader.setDRACOLoader(dracoLoader);
-    gltfloader.load( 'scenetest.glb', function ( gltf ) {
-console.log(gltf);
-        const model = gltf.scene
-        model.layers.set(0);
-        scene.add( gltf.scene );
-
-        mixer = new THREE.AnimationMixer( model );
-     
-        animate();
-
-    } );
   
-    var objBack = new THREE.Mesh(new THREE.BoxGeometry(5, 5, 1), new THREE.MeshBasicMaterial({color: "red", wireframe: false}));
-    objBack.position.z = -20    ;
-    objBack.layers.set(1);
-    scene.add(objBack);
-    const gui = new GUI();
-
-    gui.add( params, 'exposure', 0.1, 2 ).onChange( function ( value ) {
-
-        renderer.toneMappingExposure = Math.pow( value, 4.0 );
-
-    } );
-
-    gui.add( params, 'bloomThreshold', 0.0, 1.0 ).onChange( function ( value ) {
-
-        bloomPass.threshold = Number( value );
-
-    } );
-
-    gui.add( params, 'bloomStrength', 0.0, 3.0 ).onChange( function ( value ) {
-
-        bloomPass.strength = Number( value );
-
-    } );
-
-    gui.add( params, 'bloomRadius', 0.0, 1.0 ).step( 0.01 ).onChange( function ( value ) {
-
-        bloomPass.radius = Number( value );
-
-    } );
-
-    window.addEventListener( 'resize', onWindowResize );
-
-
-
-function onWindowResize() {
-
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-
-    renderer.setSize( width, height );
-    composer.setSize( width, height );
-
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
 }
 
-function animate() {
-    requestAnimationFrame(animate);
-  
-    renderer.clear();
-    
-    camera.layers.set(1);
-    composer.render();
-    
-    renderer.clearDepth();
-    camera.layers.set(0);
-    renderer.render(scene, camera);
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+   canvas: canvas
+})
+renderer.toneMapping = THREE.ReinhardToneMapping
+renderer.toneMappingExposure = 2.3
+renderer.setSize(window.innerWidth, window.innerHeight)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.physicallyCorrectLights = !0
+renderer.shadowMap.enabled=true
+// Objects
+const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
 
+// Materials
+const gltfloader = new GLTFLoader();
+var   mixer1=null
+var dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('https://raw.githubusercontent.com/mrdoob/three.js/dev/examples/js/libs/draco/');
+dracoLoader.preload();
+gltfloader.setDRACOLoader(dracoLoader);
+gltfloader.load("./CityLP_Wael.glb", function (gltf) {
+    gltf.scene.traverse(n=>{if (n.isMesh){n.castShadow=true;n.receiveShadow=true;
+    if(n.material.map){n.material.map.anisotropy=16;}
+    }})
+  console.log(gltf);
+  mixer1 = new THREE.AnimationMixer(gltf.scene);
+            const action = mixer1.clipAction(gltf.animations[0]);
+            action.play();
+  (gltf.scene.rotation.y = 3.1),
+  (gltf.scene.position.y = -3);
+  const cubeFolder1 = gui.addFolder('position');
+  cubeFolder1.add(gltf.scene.position, 'x');
+  cubeFolder1.add(gltf.scene.position, 'y');
+  cubeFolder1.add(gltf.scene.position, 'z');
+  cubeFolder1.add(gltf.scene.rotation, 'x');
+  cubeFolder1.add(gltf.scene.rotation, 'y');
+  cubeFolder1.add(gltf.scene.rotation, 'z');
+  cubeFolder1.open();
+    // gltf.scene.position.set(pos_x, pos_y, pos_z),
+    // gltf.scene.scale.set(scale_x, scale_y, scale_z),
+    (gltf.scene.castShadow = !0),
+    (gltf.scene.receiveShadow = !0),
+    scene.add(gltf.scene),
+    (gltf.scene.userData.ground = !0);
+});
+
+
+//  * Sizes
+//  */
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+    renderer.gammaOutput = !0
+    renderer.gammaFactor = 2
+})
+
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(60, sizes.width / sizes.height, 1, 5000)
+camera.position.x = 5
+camera.position.y = 5
+camera.position.z = 2
+scene.add(camera)
+
+// Lights
+
+// scene.add( new THREE.AmbientLight( 0xffffff,1 ) );
+
+const hemiLight = new THREE.HemisphereLight( 0xa9a9a9a9,0xff8c33, 2 );
+// pointLight.layers.set(1)
+scene.add( hemiLight );
+
+const spotLight = new THREE.SpotLight(0xffffff,20)
+spotLight.position.set( 0, 2.6, -12.6 );
+spotLight.scale.set( 0.005, 0.005, 0.005 );
+spotLight.castShadow=true
+spotLight.shadow.bias=-0.0001;
+spotLight.shadow.mapSize.width=1024*4
+spotLight.shadow.mapSize.height=1024*4
+spotLight.shadow.bias=-0.0001;
+scene.add(spotLight)
+const spotLightHelper = new THREE.SpotLightHelper( spotLight );
+scene.add( spotLightHelper );
+const shadowCameraHelper = new THREE.CameraHelper( spotLight.shadow.camera );
+				scene.add( shadowCameraHelper );
+
+				//
+
+// Controls
+//controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+
+//  * Animate
+//  */
+// sky
+// var sky = new Sky();
+// sky.scale.setScalar(450000);
+// scene.add(sky);
+
+// var sun = new THREE.Vector3();
+
+// const effectController = {
+//     turbidity: 10,
+//     rayleigh: 3,
+//     mieCoefficient: 0.005,
+//     mieDirectionalG: 0.7,
+//     elevation: 2,
+//     azimuth: 180,
+//     exposure: renderer.toneMappingExposure,
+// };
+
+// function guiChanged() {
+//     const uniforms = sky.material.uniforms;
+//     uniforms["turbidity"].value = effectController.turbidity;
+//     uniforms["rayleigh"].value = effectController.rayleigh;
+//     uniforms["mieCoefficient"].value = effectController.mieCoefficient;
+//     uniforms["mieDirectionalG"].value =
+//         effectController.mieDirectionalG;
+
+//     const phi = THREE.MathUtils.degToRad(
+//         90 - effectController.elevation
+//     );
+//     const theta = THREE.MathUtils.degToRad(effectController.azimuth);
+
+//     sun.setFromSphericalCoords(1, phi, theta);
+
+//     uniforms["sunPosition"].value.copy(sun);
+
+//     renderer.toneMappingExposure = effectController.exposure;
+// }
+// gui.add( effectController, 'turbidity', 0.0, 20.0, 0.1 ).onChange( guiChanged );
+// gui.add( effectController, 'rayleigh', 0.0, 4, 0.001 ).onChange( guiChanged );
+// gui.add( effectController, 'mieCoefficient', 0.0, 0.1, 0.001 ).onChange( guiChanged );
+// gui.add( effectController, 'mieDirectionalG', 0.0, 1, 0.001 ).onChange( guiChanged );
+// gui.add( effectController, 'elevation', 0, 90, 0.1 ).onChange( guiChanged );
+// gui.add( effectController, 'azimuth', - 180, 180, 0.1 ).onChange( guiChanged );
+// gui.add( effectController, 'exposure', 0, 1, 0.0001 ).onChange( guiChanged );
+// guiChanged();
+
+
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update objects
+
+    // Update Orbital Controls
+    // controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+// spotLight.position.set(camera.position.x+10,camera.position.y+10,camera.position.z+10)
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
 }
+
+tick()
