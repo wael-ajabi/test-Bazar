e2dc1c37a96584eb2d9e8a9cc6e8d35ad2dde93b
wael-ajabi <90458716+wael-ajabi@users.noreply.github.com>
2022-07-25 22:10:45 +0100

    added a new loading screen

--- src/script.js
+++ src/script.js
@@ -49,13 +49,21 @@ document.addEventListener('mousemove', function(e) {
   $('.cursor').style.top = (e.pageY - 25) + 'px';
 });
 var soundcheck=true
-document.getElementById('sound').onclick=function(){ if(soundcheck) {mediaElement.pause(); soundcheck=false} 
+document.getElementById('sound').onclick=function(){ if(soundcheck) {mediaElement.pause(); soundcheck=false ;
+document.getElementById('sound').innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" clip-rule="evenodd" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2" />
+</svg>`
+'<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">\n  <path stroke-linecap="round" stroke-linejoin="round" d="M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" clip-rule="evenodd" />\n  <path stroke-linecap="round" stroke-linejoin="round" d="M17 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2" />\n</svg>'
+} 
 
-else{mediaElement.play(); soundcheck=true}
+else{mediaElement.play(); soundcheck=true;document.getElementById('sound').innerHTML=`<svg id="soundIcon" xmlns="http://www.w3.org/2000/svg" class="h-1 w-1" fill="#AB7C94" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
+<path stroke-linecap="round" stroke-linejoin="round" d="M15.536 8.464a5 5 0 010 7.072m2.828-9.9a9 9 0 010 12.728M5.586 15H4a1 1 0 01-1-1v-4a1 1 0 011-1h1.586l4.707-4.707C10.923 3.663 12 4.109 12 5v14c0 .891-1.077 1.337-1.707.707L5.586 15z" />
+</svg>`}
 
 }
 
-
+/// navbar function
              
              function init(){
                let toolTimeline = new gsap.timeline();
@@ -68,6 +76,253 @@ else{mediaElement.play(); soundcheck=true}
                }, .1, .9);
                
              };
+
+/// loading bar function   
+
+function increase() {
+  // Change the variable to modify the speed of the number increasing from 0 to (ms)
+  let SPEED = 40;
+  // Retrieve the percentage value
+  let limit = parseInt(document.getElementById("value1").innerHTML, 10);
+
+  for(let i = 0; i <= limit; i++) {
+      setTimeout(function () {
+          document.getElementById("value1").innerHTML = i + "%";
+      }, SPEED * i);
+  }
+}
+
+increase();
+
+/// loading background function
+
+/*          *     .        *  .    *    *   . 
+ .  *  move your mouse to over the stars   .
+ *  .  .   change these values:   .  *
+   .      * .        .          * .       */
+   const STAR_COLOR = '#fff';
+   const STAR_SIZE = 3;
+   const STAR_MIN_SCALE = 0.2;
+   const OVERFLOW_THRESHOLD = 50;
+   const STAR_COUNT = ( window.innerWidth + window.innerHeight ) / 8;
+   
+   var canvas2 = document.getElementById('load'),
+         context = canvas2.getContext( '2d' );
+   
+   let scale = 1, // device pixel ratio
+       width,
+       height;
+   
+   let stars = [];
+   
+   let pointerX,
+       pointerY;
+   
+   let velocity = { x: 0, y: 0, tx: 0, ty: 0, z: 0.0005 };
+   
+   let touchInput = false;
+   
+   generate();
+   resize();
+   step();
+   
+   window.onresize = resize;
+   canvas2.onmousemove = onMouseMove;
+   canvas2.ontouchmove = onTouchMove;
+   canvas2.ontouchend = onMouseLeave;
+   document.onmouseleave = onMouseLeave;
+   
+   function generate() {
+   
+      for( let i = 0; i < STAR_COUNT; i++ ) {
+       stars.push({
+         x: 0,
+         y: 0,
+         z: STAR_MIN_SCALE + Math.random() * ( 1 - STAR_MIN_SCALE )
+       });
+      }
+   
+   }
+   
+   function placeStar( star ) {
+   
+     star.x = Math.random() * width;
+     star.y = Math.random() * height;
+   
+   }
+   
+   function recycleStar( star ) {
+   
+     let direction = 'z';
+   
+     let vx = Math.abs( velocity.x ),
+         vy = Math.abs( velocity.y );
+   
+     if( vx > 1 || vy > 1 ) {
+       let axis;
+   
+       if( vx > vy ) {
+         axis = Math.random() < vx / ( vx + vy ) ? 'h' : 'v';
+       }
+       else {
+         axis = Math.random() < vy / ( vx + vy ) ? 'v' : 'h';
+       }
+   
+       if( axis === 'h' ) {
+         direction = velocity.x > 0 ? 'l' : 'r';
+       }
+       else {
+         direction = velocity.y > 0 ? 't' : 'b';
+       }
+     }
+     
+     star.z = STAR_MIN_SCALE + Math.random() * ( 1 - STAR_MIN_SCALE );
+   
+     if( direction === 'z' ) {
+       star.z = 0.1;
+       star.x = Math.random() * width;
+       star.y = Math.random() * height;
+     }
+     else if( direction === 'l' ) {
+       star.x = -OVERFLOW_THRESHOLD;
+       star.y = height * Math.random();
+     }
+     else if( direction === 'r' ) {
+       star.x = width + OVERFLOW_THRESHOLD;
+       star.y = height * Math.random();
+     }
+     else if( direction === 't' ) {
+       star.x = width * Math.random();
+       star.y = -OVERFLOW_THRESHOLD;
+     }
+     else if( direction === 'b' ) {
+       star.x = width * Math.random();
+       star.y = height + OVERFLOW_THRESHOLD;
+     }
+   
+   }
+   
+   function resize() {
+   
+     scale = window.devicePixelRatio || 1;
+   
+     width = window.innerWidth * scale;
+     height = window.innerHeight * scale;
+   
+     canvas2.width = width;
+     canvas2.height = height;
+   
+     stars.forEach( placeStar );
+   
+   }
+   
+   function step() {
+   
+     context.clearRect( 0, 0, width, height );
+   
+     update();
+     render();
+   
+     requestAnimationFrame( step );
+   
+   }
+   
+   function update() {
+   
+     velocity.tx *= 0.96;
+     velocity.ty *= 0.96;
+   
+     velocity.x += ( velocity.tx - velocity.x ) * 0.8;
+     velocity.y += ( velocity.ty - velocity.y ) * 0.8;
+   
+     stars.forEach( ( star ) => {
+   
+       star.x += velocity.x * star.z;
+       star.y += velocity.y * star.z;
+   
+       star.x += ( star.x - width/2 ) * velocity.z * star.z;
+       star.y += ( star.y - height/2 ) * velocity.z * star.z;
+       star.z += velocity.z;
+     
+       // recycle when out of bounds
+       if( star.x < -OVERFLOW_THRESHOLD || star.x > width + OVERFLOW_THRESHOLD || star.y < -OVERFLOW_THRESHOLD || star.y > height + OVERFLOW_THRESHOLD ) {
+         recycleStar( star );
+       }
+   
+     } );
+   
+   }
+   
+   function render() {
+   
+     stars.forEach( ( star ) => {
+   
+       context.beginPath();
+       context.lineCap = 'round';
+       context.lineWidth = STAR_SIZE * star.z * scale;
+       context.globalAlpha = 0.5 + 0.5*Math.random();
+       context.strokeStyle = STAR_COLOR;
+   
+       context.beginPath();
+       context.moveTo( star.x, star.y );
+   
+       var tailX = velocity.x * 2,
+           tailY = velocity.y * 2;
+   
+       // stroke() wont work on an invisible line
+       if( Math.abs( tailX ) < 0.1 ) tailX = 0.5;
+       if( Math.abs( tailY ) < 0.1 ) tailY = 0.5;
+   
+       context.lineTo( star.x + tailX, star.y + tailY );
+   
+       context.stroke();
+   
+     } );
+   
+   }
+   
+   function movePointer( x, y ) {
+   
+     if( typeof pointerX === 'number' && typeof pointerY === 'number' ) {
+   
+       let ox = x - pointerX,
+           oy = y - pointerY;
+   
+       velocity.tx = velocity.tx + ( ox / 8*scale ) * ( touchInput ? 1 : -1 );
+       velocity.ty = velocity.ty + ( oy / 8*scale ) * ( touchInput ? 1 : -1 );
+   
+     }
+   
+     pointerX = x;
+     pointerY = y;
+   
+   }
+   
+   function onMouseMove( event ) {
+   
+     touchInput = false;
+   
+     movePointer( event.clientX, event.clientY );
+   
+   }
+   
+   function onTouchMove( event ) {
+   
+     touchInput = true;
+   
+     movePointer( event.touches[0].clientX, event.touches[0].clientY, true );
+   
+     event.preventDefault();
+   
+   }
+   
+   function onMouseLeave() {
+   
+     pointerX = null;
+     pointerY = null;
+   
+   }
+   
 
 
 
@@ -303,14 +558,13 @@ function ( error ) {
 });
 document.getElementById('start-button').style.display='none'
 
-setTimeout(()=>{    document.getElementById( 'loading-screen' ).style.display='none' ;     document.getElementById('start-button').style.display='block'
-init();
+setTimeout(()=>{    document.getElementById( 'chart' ).style.display='none' ;     document.getElementById('start-button').style.display='block'
 
-},4000)
+},9000)
 //loading
 const loadingManager = new THREE.LoadingManager( () => {
 	
-  const loadingScreen = document.getElementById( 'loading-screen' );
+  const loadingScreen = document.getElementById( 'chart' );
   loadingScreen.classList.add( 'fade-out' );
   
   // optional: remove loader from DOM via event listener
@@ -880,8 +1134,11 @@ var mediaElement=null;
 var myInterval=null
 document.getElementById('start-button').onclick=function(){
   controls.enabled=false
-
+  init();
+  var element = document.getElementById('load')
+element.className = "myelement";
     document.getElementById('start-button').style.display='none'
+    // document.getElementById('load').style.display='none'
      myInterval = setInterval(()=>{if(fog.density>0.39){return} ; if (fog.density<0.4){fonintensity+=0.01;fog.density=fonintensity }}, 100);
 
      
@@ -917,7 +1174,7 @@ document.getElementById('start-button').onclick=function(){
       gsap.to(camera.position,{x: -0.9368241147924617, y: 0.44978222785543465, z: -1.7328521118110993,duration:15,delay:8,onComplete:function(){
         camerarotation= true;
         controls.maxDistance=4
-
+        // canvas2 = null
 
         var bokehPass = new BokehPass(scene, camera, {
           focus: 8,
