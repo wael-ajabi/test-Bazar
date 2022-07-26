ffab25373f37a804c3e575c0958b8d7660de47f7
wael-ajabi <90458716+wael-ajabi@users.noreply.github.com>
2022-07-26 15:39:39 +0100

    added eye icon

--- src/script.js
+++ src/script.js
@@ -229,11 +229,11 @@ increase();
    
    function update() {
    
-     velocity.tx *= 0.96;
-     velocity.ty *= 0.96;
+     velocity.tx *= 0.3;
+     velocity.ty *= 0.3;
    
-     velocity.x += ( velocity.tx - velocity.x ) * 0.8;
-     velocity.y += ( velocity.ty - velocity.y ) * 0.8;
+     velocity.x += ( velocity.tx - velocity.x ) * 0.1;
+     velocity.y += ( velocity.ty - velocity.y ) * 0.1;
    
      stars.forEach( ( star ) => {
    
