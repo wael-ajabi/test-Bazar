91c90e87b8fb22c53eeb044f375de664af77ee7a
wael-ajabi <90458716+wael-ajabi@users.noreply.github.com>
2022-08-07 17:16:33 +0100

    added the game

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -4,15 +4,8 @@ const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 module.exports = {
-    entry: {main:path.resolve(__dirname, '../src/script.js'),
-    second:path.resolve(__dirname, '../src/sketch.js'),
-    zebi:{
-        import:path.resolve(__dirname, '../src/sketch.js'),
-        library:{
-            name:'zebi',
-            type:'var'
-        }
-    }},
+    entry: [path.resolve(__dirname, '../src/script.js'),path.resolve(__dirname, '../src/sketch.js')],
+
     output:
     {
         filename: 'bundle.[contenthash].js',
