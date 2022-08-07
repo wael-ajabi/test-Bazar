427dbd60e67ee1da706e5c64b961bf18c8e88ded
wael-ajabi <90458716+wael-ajabi@users.noreply.github.com>
2022-08-07 14:23:08 +0100

    added the game

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -4,7 +4,7 @@ const MiniCSSExtractPlugin = require('mini-css-extract-plugin')
 const path = require('path')
 
 module.exports = {
-    entry: '../src/script.js',
+    entry: path.resolve(__dirname, ['../src/script.js','../src/sketch.js']),
     
     output:
     {
