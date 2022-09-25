65a7b866044f4f77413b343940feda0667500482
wael-ajabi <90458716+wael-ajabi@users.noreply.github.com>
2022-09-25 01:16:01 +0100

    testing the eye icon

--- src/script.js
+++ src/script.js
@@ -105,7 +105,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   if (window.innerWidth < 700) {
-    tops=0
+    tops=-60
 
   document.getElementById('sound').innerHTML=`
   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
